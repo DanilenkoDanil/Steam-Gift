d3df19bcb0a92a0f5b54768e0ad1ec38892fee20
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2021-10-03 19:40:20 +0300

    Update send_gift.py

--- main/send_gift.py
+++ main/send_gift.py
@@ -15,7 +15,11 @@ from pyvirtualdisplay import Display
 def steam_login(driver, login: str, password: str):
     driver.get('http://steamcommunity.com/login/home/?goto=')
     time.sleep(10)
-    login_input = driver.find_element_by_xpath('//*[@id="input_username"]')
+    try:
+        login_input = driver.find_element_by_xpath('//*[@id="input_username"]')
+    except NoSuchElementException:
+        time.sleep(15)
+        login_input = driver.find_element_by_xpath('//*[@id="input_username"]')
     for i in login:
         login_input.send_keys(i)
         time.sleep(random.uniform(0, 0.2))
