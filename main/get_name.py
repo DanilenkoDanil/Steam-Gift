032b25331dcf007690ff3b3a4a9cfedd666ab1f2
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2021-10-24 15:42:38 +0300

    Update get_name.py

--- main/get_name.py
+++ main/get_name.py
@@ -1,7 +1,14 @@
 import requests
+import steamfront
 
 
 def get_name(link):
     res = requests.get(link)
     name = str(res.content).replace('\\t', '').replace('\\r', '').replace('\\n', '').split('Steam Community :: ')[1].split('</title>')[0]
     return name
+
+
+def get_name_game(app_code):
+    client = steamfront.Client()
+    game = client.getApp(appid=app_code)
+    return game.name
