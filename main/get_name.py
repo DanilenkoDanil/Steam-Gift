fc5f9c15866e74265c5deb5c1bf82af70a9099b8
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2021-11-08 02:58:57 +0200

    Update get_name.py

--- main/get_name.py
+++ main/get_name.py
@@ -1,5 +1,6 @@
 import requests
 import steamfront
+import json
 
 
 def get_name(link):
@@ -8,7 +9,16 @@ def get_name(link):
     return name
 
 
-def get_name_game(app_code):
-    client = steamfront.Client()
-    game = client.getApp(appid=app_code)
-    return game.name
+def get_name_game(sub_id):
+    link = f'http://store.steampowered.com/api/packagedetails?packageids={sub_id}'
+    res = requests.get(link)
+    result_dict = json.loads(res.content.decode('utf-8'))
+    try:
+        print(sub_id)
+        print(result_dict)
+        return result_dict[str(sub_id)]['data']['name']
+    except KeyError:
+        return "Не удалось получить имя"
+
+
+# print(get_name_game('244390'))
