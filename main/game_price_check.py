28de25e41c7265b83dcbfc9850071e55fd242934
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2021-11-08 02:59:37 +0200

    Update game_price_check.py

--- main/game_price_check.py
+++ main/game_price_check.py
@@ -2,14 +2,14 @@ import requests
 import json
 
 
-def get_price(app_id):
-    url = "http://store.steampowered.com/api/appdetails/?appids={}".format(app_id)
-    response = requests.get(url)
-    j = response.json()
-    pre_final_price = j["{}".format(app_id)]["data"]["price_overview"]["final"]
-    final_price = float(pre_final_price) / 100
+def get_price(sub_id):
+    link = f'http://store.steampowered.com/api/packagedetails?packageids={sub_id}'
+    res = requests.get(link)
+    result_dict = json.loads(res.content.decode('utf-8'))
+    try:
+        return int(result_dict[str(sub_id)]['data']['price']['final'])/100
+    except KeyError:
+        return 0
 
-    return final_price
 
-
-# print(get_price(644560))
+# +
