df356132e8f7503ee549a9afdba22ae6fea07e71
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2021-11-22 05:40:16 +0200

    Update settings.py

--- gift_bot/settings.py
+++ gift_bot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pn#or9b@32%a86#8xc$h-$y-sfciez8!lokadl3@5n3**(zr#a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['checfory.com']
 
 
 # Application definition
