c80a11ab69f4b6f3eecdefca950a7c352448f341
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2021-11-22 06:18:59 +0200

    Update urls.py

--- gift_bot/urls.py
+++ gift_bot/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django_otp.admin import OTPAdminSite
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
 ]
+
+admin.site.__class__ = OTPAdminSite
