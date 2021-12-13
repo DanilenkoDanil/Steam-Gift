8fb82b31a8f0ae37906e7963b305485973c74c61
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2021-12-13 18:00:43 +0200

    Update get_name.py

--- main/get_name.py
+++ main/get_name.py
@@ -5,7 +5,7 @@ import json
 
 def get_name(link):
     res = requests.get(link)
-    name = str(res.content).replace('\\t', '').replace('\\r', '').replace('\\n', '').split('Steam Community :: ')[1].split('</title>')[0]
+    name = str(res.text).replace('\\t', '').replace('\\r', '').replace('\\n', '').split('Steam Community :: ')[1].split('</title>')[0]
     return name
 
 
